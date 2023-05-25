dec66dc9e32a3688a0338ac587ff14ca23463ab8
Niti1605 <106881020+Niti1605@users.noreply.github.com>
2023-05-25 20:26:17 +0530

    initial

---
+++ assingment1.php
@@ -0,0 +1,21 @@
+<?php
+$a=10;
+$b=25;
+$c=20;
+
+if($a>$b)
+{
+if($a>$c)
+{
+echo "a is the biggest number";
+}
+else
+{
+echo "c is the biggest number";
+}
+}
+if($b>$c)
+{
+ echo "b is the biggest number";
+}
+?>
